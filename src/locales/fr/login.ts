83693651b4ae095b442f8ca43aae25f4b4df3153
pauloromi <hello@pauloromi.com>
2026-01-16 18:44:19 +0100

    sorry vercel

--- src/locales/fr/login.ts
+++ src/locales/fr/login.ts
@@ -1,5 +1,20 @@
 export const login = {
   sectionLabel: "Connexion",
   title: "Connexion simple",
-  description: "Connexion simple via email."
+  description: "Connexion simple via email.",
+  form: {
+    label: "Email",
+    placeholder: "toi@exemple.com",
+    button: {
+      envoyer: "Recevoir le lien",
+      envoi: "Envoi...",
+    },
+    success: {
+      message: "Lien envoyé. Vérifie ta boîte mail.",
+      toast: "Lien de connexion envoyé.",
+    },
+    error: {
+      toast: "Impossible d’envoyer le lien. Réessaie.",
+    },
+  },
 };
